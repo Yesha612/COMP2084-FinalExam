0bfe3da2a8c515909d8b5da68c3af2302c15649d
Yesha612 <yesha06121999@gmail.com>
2019-04-17 13:32:36 -0400

    Almost done with finals

---
+++ VbFinal.Tests/Controllers/VbPlayersControllerTest.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using VbFinal.Controllers;
+using VbFinal.Models;
+
+// new references
+using System.Web.Mvc;
+
+namespace VbFinal.Tests.Controllers
+{
+    [TestClass]
+    public class VbPlayersControllerTest
+    {
+        VbPlayersController controller;
+        Mock<IMockVbPlayer> mock;
+        List<VbPlayer> vbPlayers;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            mock = new Mock<IMockVbPlayer>();
+
+            vbPlayers = new List<VbPlayer>
+            {
+                new VbPlayer
+                {
+                    VbPlayerId = 961,
+                    FirstName = "Lori",
+                    Lastname = "S",
+                    Photo = "https://img.icons8.com/color/384/beach-volleyball.png"
+                },
+                new VbPlayer
+                {
+                    VbPlayerId = 961,
+                    FirstName = "Zak",
+                    Lastname = "N",
+                    Photo = "https://img.icons8.com/ultraviolet/384/beach-volleyball.png"
+                }
+            };
+
+            mock.Setup(m => m.VbPlayers).Returns(vbPlayers.AsQueryable());
+            controller = new VbPlayersController(mock.Object);
+        }
+
+        // Index
+        [TestMethod]
+        public void IndexViewLoads()
+        {
+            // Arrange
+            //Handled in TestInitiliaze
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        [TestMethod]
+        public void IndexValidLoadsVbPlayers()
+        {
+            // Arrange
+            //Handled in TestInitiliaze
+
+            // Act
+            var result = (List<VbPlayer>)((ViewResult)controller.Index()).Model;
+
+            // Assert
+            CollectionAssert.AreEqual(vbPlayers.ToList(), result);
+        }
+
+        // Edit
+        [TestMethod]
+        public void EditLoadsValidId()
+        {
+            // Arrange
+            //Handled in TestInitiliaze
+
+            // Act
+            ViewResult result = controller.Edit(961) as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Edit", result.ViewName);
+
+        }
+
+        [TestMethod]
+        public void EditLoadsVbPlayerValidId()
+        {
+            // Arrange
+            //Handled in TestInitiliaze
+
+            // Act
+
+            // Assert
+
+        }
+
+        [TestMethod]
+        public void EditInvalidId()
+        {
+            // Arrange
+            //Handled in TestInitiliaze
+
+            // Act
+            HttpNotFoundResult result = controller.Edit(500) as HttpNotFoundResult;
+
+            // Assert
+            Assert.AreEqual(404, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void EditNoId()
+        {
+            // Arrange
+            //Handled in TestInitiliaze
+
+            // Act
+            //HttpNotFoundResult result = controller.Edit(null) as HttpNotFoundResult;
+
+            // Assert
+            //Assert.AreEqual(404, result.StatusCode);
+
+        }
+
+        [TestMethod]
+        public void EditSaveInvalid()
+        {
+            // Arrange
+            //Handled in TestInitiliaze
+
+            // Act
+
+            // Assert
+
+        }
+
+        [TestMethod]
+        public void EditSaveValid()
+        {
+            // Arrange
+            //Handled in TestInitiliaze
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
